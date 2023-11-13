f49cab9e9146770ccc4abd672a4dd41f282ca965
HaiderAli00033 <minestudy142@gmail.com>
2023-11-13 09:45:52 -0800

    Its now

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", function () {
     var navbar = document.querySelector(".nav");
 
-    if (window.scrollY > 100) {
+    if (window.scrollY > 150) {
         navbar.classList.add("nav-hidden");
     } else {
         navbar.classList.remove("nav-hidden");
